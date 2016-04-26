5f7a74029105d546eaecfea33e9118511cc05c84
Christiaan van Tienhoven <tienhoven.c@tkppensioen.nl>
2016-04-26 15:54:57 +0200

    Updated config

--- hello.go
+++ hello.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
-	config := tray_plugin.Config{{"url", "http://sensu.prd.intra.tkppensioen.nl:4567/results"}}
+	config := tray_plugin.Config{{"url", "http://sensu.prd.intra.tkppensioen.nl:4567/events"}, {"tag", "type=system"}, {"subscription", "owner=pdp"}}
 	tray_sensu_plugin.GetStatus(config)
 }
